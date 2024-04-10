f0de46586d53ab3dd13302c844afe60291d16601
ricky <39970880+torickyto@users.noreply.github.com>
2024-04-09 21:10:52 -0600

    updating homepage UI

--- app/page.js
+++ app/page.js
@@ -21,8 +21,8 @@ export default function Home() {
         <title>dot cussy</title>
       </Head>
       <header className="fixed top-0 left-0 w-full flex items-center px-4 z-10" style={{ height: '16vh', background: 'linear-gradient(to bottom, #333, #000)' }}>
-        <img src="/dotcussyPatch.png" alt="logo" className="mr-2" style={{ height: '100%', marginLeft: '-84px'}} />
-        <div className=" -ml-44 flex justify-around items-center h-full w-full">
+        <img src="/dotcussyPatch.png" alt="logo" className="mr-2" style={{ height: '100%', marginLeft: '-64px'}} />
+        <div className=" -ml-40 flex justify-around items-center h-full w-full mr-64">
           <div className="pt-14 text-center">
             <Link legacyBehavior href="/ourArtist">
               <a className="text-7xl font-bold text-gray-100">OUR ARTISTS</a>
@@ -44,17 +44,24 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/* Removed Sign Up button, left only Sign In */}
         <div className="absolute top-4 right-4">
           <button className=" rounded-3xl bg-green-500 text-gray-100 px-4 py-2">sign in</button>
         </div>
       </header>
-      <main className="grid place-items-center" style={{ height: 'calc(100vh - 20vh)' }}>
-        <div>
-          <h1 className="text-6xl font-semibold text-gray-100 mb-2">Local Roots. Global Reach.</h1>
-          <h3 className="text-3xl text-center text-gray-100">From YYC to the World.</h3>
-        </div>
-      </main>
+      <main className="flex flex-col items-center justify-center h-screen" style={{ marginTop: '-5%' }}>
+  <div className="text-center px-4 space-y-2 md:space-y-3">
+    <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold text-gray-100">
+    <div className="flex justify-center text-4xl md:text-5xl lg:text-6xl font-semibold text-gray-100">
+  <span style={{ background: 'linear-gradient(to right, darkgray, lightgray)', WebkitBackgroundClip: 'text', color: 'transparent' }}>Local Roots.</span>
+  <span className="pl-2">Global Reach.</span>
+</div>
+    </h1>
+    <h3 className="text-2xl md:text-3xl lg:text-4xl text-gray-100 pb-2 font-sem">From <span style={{ color: '#1DB954' }}>YYC</span> to the World.</h3>
+    <img src="/spotifylogo.png" alt="Spotify Logo" className="mx-auto" style={{ maxWidth: '60px', height: 'auto', marginBottom: '85px' }}/>
+    <h2 className="text-3xl md:text-4xl lg:text-5xl text-gray-100">We're scouting. Are you up?</h2>
+  </div>
+  <button className="text-xl md:text-2xl lg:text-3xl rounded-3xl bg-green-500 text-gray-100 px-12 py-2 mt-6">sign up</button>
+</main>
     </>
   );
 }
