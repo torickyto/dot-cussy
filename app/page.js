69b6bf6ce61486df64b49232e4952d355b5022ae
ricky <39970880+torickyto@users.noreply.github.com>
2024-04-10 20:07:52 -0600

    finished home ui

--- app/page.js
+++ app/page.js
@@ -25,17 +25,17 @@ export default function Home() {
         <div className=" -ml-40 flex justify-around items-center h-full w-full mr-64">
           <div className="pt-14 text-center">
             <Link legacyBehavior href="/ourArtist">
-              <a className="text-7xl text-gray-100 font-semibold">OUR ARTISTS</a>
+              <a className="text-7xl text-gray-100 font-semibold text-shadow-right">OUR ARTISTS</a>
             </Link>
-            <div className="mt-2">
+            <div className="mt-2 text-shadow-right-small">
               <Link legacyBehavior href="/artist1"><a className="text-2xl text-gray-100 mx-2">TRENCHCOATGORDY</a></Link>
               <Link legacyBehavior href="/artist2"><a className="text-2xl text-gray-100 mx-2">DEATHCON3</a></Link>
               <Link legacyBehavior href="/artist3"><a className="text-2xl text-gray-100 mx-2">BANFFWILLY</a></Link>
             </div>
           </div>
-          <div className="pt-14 text-center">
+          <div className="pt-14 text-center text-shadow-left-small">
             <Link legacyBehavior href="/aboutUs">
-              <a className="text-7xl text-gray-100 font-semibold">ABOUT US</a>
+              <a className="text-7xl text-gray-100 font-semibold text-shadow-left">ABOUT US</a>
             </Link>
             <div className="mt-2">
               <Link legacyBehavior href="/faq"><a className="text-2xl text-gray-100 mx-2">FAQ</a></Link>
@@ -45,23 +45,23 @@ export default function Home() {
           </div>
         </div>
         <div className="absolute top-4 right-4">
-          <button className=" rounded-3xl bg-green-500 text-gray-100 px-4 py-2">sign in</button>
+        <button class="button-3d carved-text text-xl md:text-2xl lg:text-3xl px-12 py-2 mt-6">sign in</button>
         </div>
       </header>
       <main className="flex flex-col items-center justify-center h-screen" style={{ marginTop: '-5%' }}>
   <div className="text-center px-4 space-y-2 md:space-y-3">
     <h1 className="text-3xl md:text-4xl lg:text- text-gray-100">
     <div className="flex justify-center text-4xl md:text-5xl lg:text-6xl text-gray-100">
+    <span className="text-shadow">
   <span style={{ background: 'linear-gradient(to right, darkgray, lightgray)', WebkitBackgroundClip: 'text', color: 'transparent' }}>Local Roots.</span>
-  <span className="pl-2">Global Reach.</span>
+  <span className="pl-2">Global Reach.</span></span>
 </div>
     </h1>
-    <h3 className="text-2xl md:text-3xl lg:text-4xl text-gray-100 pb-2 font-sem">From <span style={{ color: '#1DB954' }}>YYC</span> to the World.</h3>
+    <h3 className="text-2xl md:text-3xl lg:text-4xl text-gray-300 pb-2 font-sem">From <span style={{ color: '#1DB954' }}>YYC</span> to the World.</h3>
     <img src="/spotifylogo.png" alt="Spotify Logo" className="mx-auto" style={{ maxWidth: '60px', height: 'auto', marginBottom: '85px' }}/>
     <h2 className="text-3xl md:text-4xl lg:text-5xl text-gray-100">We're scouting. Are you up?</h2>
   </div>
-  <button className="text-xl md:text-2xl lg:text-3xl rounded-3xl bg-green-500 text-gray-100 px-12 py-2 mt-6">sign up</button>
-</main>
+  <button class="button-3d carved-text text-xl md:text-2xl lg:text-3xl px-12 py-2 mt-6">sign up</button></main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +15,26 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow': {
+          textShadow: '0px 3px 3px rgba(100, 100, 100, 1)',
+        },
+        '.text-shadow-right': {
+          textShadow: '6px 3px 3px rgba(100, 100, 100, 1)',
+        },
+        '.text-shadow-right-small': {
+          textShadow: '4px 3px 3px rgba(120, 120, 120, 1)',
+        },
+        '.text-shadow-left': {
+          textShadow: '-6px 3px 3px rgba(100, 100, 100, 1)',
+        },
+        '.text-shadow-left-small': {
+          textShadow: '-3px 2px 3px rgba(120, 120, 120, 1)',
+        },
+      }
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    }),
+  ],
 };
